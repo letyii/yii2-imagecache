0848f045dcda8beb5b937e17e5bc3d658024d535
NguaGo <nguago@let.vn>
2015-04-25 10:10:38 +0700

    Update imageCache file and Readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # yii2-imagecache
+
+## Installation
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require "letyii/yii2-imagecache" "dev-master"
+```
+or add
+
+```json
+"letyii/yii2-imagecache": "dev-master"
+```
+
+to the require section of your application's `composer.json` file.
+
+## Config
+~~~php
+'components' => [
+    ...
+    'imageCache' => [
+        'class' => 'letyii\imagecache\ImageCache',
+        'cachePath' => '@app/uploads/cache',
+        'cacheUrl' => '@web/uploads/cache',
+    ],
+]
+~~~
+
+## Usage Example
+~~~php
+<?= Yii::$app->imageCache->imgSrc('@web/uploads/test.jpg', 'x200') ?>
+// output: /your-app/uploads/cache/x200/.../test.jpg
+
+<?= Yii::$app->imageCache->img('@web/uploads/test.jpg', '100x') ?>
+// <img src="/your-app/uploads/cache/100x/.../test.jpg" alt="">
+
+<?= Yii::$app->imageCache->img('@web/uploads/test.jpg', '100x150', ['class'=>'test', 'alt' => 'Test image']) ?>
+// <img src="/your-app/uploads/cache/100x120/.../test.jpg" alt="" class="img" alt="Test image">
+~~~
--- imageCache.php
+++ imageCache.php
@@ -2,17 +2,121 @@
 
 namespace letyii\imagecache;
 
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\BaseFileHelper;
+
 class ImageCache extends \yii\base\Component
 {
+
+    public $defaultSize = '800x';
+
+    public $cachePath;
+    
+    public $cacheUrl;
+    
+    public $graphicsLibrary = 'Imagick';
+    
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if (!isset($this->cachePath))
+            throw new \yii\base\InvalidConfigException('Please, set "cachePath" at $config["components"]["imageCache"]["cachePath"].');
+        
+        $this->cachePath = Yii::getAlias($this->cachePath);
+    }
+    /**
+     * Get thumbnail
+     * @param string $srcImagePath
+     * @param string $size
+     * @param array $options the tag options in terms of name-value pairs. These will be rendered as
+     * @return string
+     */
+    public function img($srcImagePath, $size = null, $options = []) {
+        return Html::img(self::imgSrc($srcImagePath, $size), $options);
+    }
+    
+    public function imgSrc($srcImagePath, $size = null) {
+        $srcImagePath = Yii::getAlias($srcImagePath);
+        
+        // Check whether there is a source file
+        if(!is_file($srcImagePath))
+            return null;
+
+        // Check whether there is an encrypted file
+        if(is_file($this->getCachedFile($srcImagePath, $size, 'path')))
+            return $this->getCachedFile($srcImagePath, $size);
+        else
+            return null;
+    }
+    
+    private function getCachedFile($srcImagePath, $size, $type = 'url') {
+        $file = pathinfo($srcImagePath);
+        if(!$file['basename'])
+            return fasle;
+        
+        $file = $this->getDir($srcImagePath, $size) . DIRECTORY_SEPARATOR . $file['basename'];
+        $cacheFilePath = $this->cachePath . DIRECTORY_SEPARATOR . $file;
+        if (!is_file($cacheFilePath))
+            $this->createCachedFile ($srcImagePath, $cacheFilePath, $size);
+            
+        if ($type == 'path')
+            return $cacheFilePath;
+        elseif ($type == 'url')
+            return $this->cacheUrl . DIRECTORY_SEPARATOR . $file;
+        else
+            return null;
+    }
+    
+    private function getDir ($srcImagePath, $size = null) {
+        $md5FileName = md5($srcImagePath);
+        $dir = substr($md5FileName, 0, 2) . DIRECTORY_SEPARATOR . substr($md5FileName, 2, 2) . DIRECTORY_SEPARATOR . substr($md5FileName, 4, 2);
+        if ($size)
+            $dir = $size . DIRECTORY_SEPARATOR . $dir;
+        return $dir;
+    }
+
+    /**
+     * @param $srcImagePath
+     * @param bool $preset
+     * @return string Path to cached file
+     * @throws \Exception
+     */
+    private function createCachedFile($srcImagePath, $pathToSave, $size = null)
+    {
+        BaseFileHelper::createDirectory(dirname($pathToSave), 0777, true);
+        $size = $size ? $this->parseSize($size) : false;
+//        if($this->graphicsLibrary == 'Imagick'){
+            $image = new \Imagick($srcImagePath);
+            $image->setImageCompressionQuality(100);
+            if($size){
+                if($size['height'] && $size['width']){
+                    $image->cropThumbnailImage($size['width'], $size['height']);
+                }elseif($size['height']){
+                    $image->thumbnailImage(0, $size['height']);
+                }elseif($size['width']){
+                    $image->thumbnailImage($size['width'], 0);
+                }else{
+                    throw new \Exception('Error at $this->parseSize($sizeString)');
+                }
+            }
+            $image->writeImage($pathToSave);
+//        }
+        if(!is_file($pathToSave))
+            throw new \Exception('Error while creating cached file');
+        return $image;
+    }
 
     /**
      * Parses size string
      * For instance: 400x400, 400x, x400
-     *
      * @param $sizeString
      * @return array|null
      */
-    public function parseSize($sizeString)
+    private function parseSize($sizeString)
     {
         if(!$sizeString)
             $sizeString = $this->defaultSize;
@@ -46,4 +150,5 @@ class ImageCache extends \yii\base\Component
         }
         return $size;
     }
+    
 }
